58b927bde87b7b3d662d1a8f5347386bd9764d7a
dave <dave.dega@gmail.com>
2018-02-04 14:34:25 +0100

    push project

---
+++ app/src/main/java/com/dega/stories/StoriesActivity.kt
@@ -0,0 +1,12 @@
+package com.dega.stories
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class StoriesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stories)
+    }
+}
